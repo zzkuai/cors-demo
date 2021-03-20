24cf141d8dcfd6e69dcd4900c93fcea8ca53322f
zzkuai <zzkuai@gmail.com>
2021-03-20 17:26:36 +0800

    fix: 调整描述

--- server/index.js
+++ server/index.js
@@ -2,19 +2,20 @@ const http = require("http");
 
 http
   .createServer((req, res) => {
-    // 允许跨域访问的域名
-    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-
-    // 非简单请求下额外响应头
     // 非简单请求指：
     // 1.method 是 GET POST HEAD 且不带自定义request header
     // 2.content-type 为 application/x-www-form-urlencoded multipart/form-data text/plain
+
+    // 允许跨域请求的前端域名
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+
+    // 非简单请求下允许的额外请求头
     res.setHeader("Access-Control-Allow-Headers", "content-type, x-key");
 
     // cookie 设置
     res.setHeader("Access-Control-Allow-Credentials", true);
 
-    // 允许前端获取 res 的 header
+    // 允许前端获取的响应头
     res.setHeader("Access-Control-Expose-Headers", "x-version");
 
     // 非简单请求场景下 预检请求的缓存时间 单位 s
