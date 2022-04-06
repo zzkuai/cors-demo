330a092be0e4f9f93e8e921eb770e07c4876779a
zzkuai <zzkuai@gmail.com>
2022-04-06 23:00:55 +0800

    feat: 添加 jsonp demo

--- server/index.js
+++ server/index.js
@@ -3,30 +3,30 @@ const express = require('express');
 const app = express();
 
 app.use('*', (req, res, next) => {
-  res.set({
-    // 允许跨域请求的前端域名
-    'Access-Control-Allow-Origin': req.headers.origin,
+  // res.set({
+  //   // 允许跨域请求的前端域名
+  //   'Access-Control-Allow-Origin': req.headers.origin,
 
-    // 允许跨域请求方法
-    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  //   // 允许跨域请求方法
+  //   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
 
-    // 非简单请求下允许的额外请求头
-    'Access-Control-Allow-Headers': 'content-type, x-key',
+  //   // 非简单请求下允许的额外请求头
+  //   'Access-Control-Allow-Headers': 'content-type, x-key',
 
-    // cookie 设置
-    'Access-Control-Allow-Credentials': true,
+  //   // cookie 设置
+  //   'Access-Control-Allow-Credentials': true,
 
-    // 允许前端获取的响应头
-    'Access-Control-Expose-Headers': 'x-version',
+  //   // 允许前端获取的响应头
+  //   'Access-Control-Expose-Headers': 'x-version',
 
-    // 非简单请求场景下 预检请求的缓存时间 单位 s
-    'Access-Control-Max-Age': 10,
+  //   // 非简单请求场景下 预检请求的缓存时间 单位 s
+  //   'Access-Control-Max-Age': 10,
 
-    'Content-Type': 'application/json',
+  //   'Content-Type': 'application/json',
 
-    // 自定义 res header
-    'x-version': 1.0,
-  });
+  //   // 自定义 res header
+  //   'x-version': 1.0,
+  // });
 
   res.cookie('myCookie', 'test');
 
@@ -38,6 +38,19 @@ app.get('/', (req, res) => {
     name: 'zzkuai',
     age: 24,
   });
+});
+
+app.get('/data', (req, res) => {
+  const { id, callback } = req.query;
+  const result = {
+    name: 'zzkuai',
+    age: 25,
+  };
+
+  console.log('id', id);
+  console.log('callback', callback);
+
+  res.end(`${callback}(${JSON.stringify(result)})`);
 });
 
 app.listen(3000, '127.0.0.1', () => {
