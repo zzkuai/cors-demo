5fe8906390dcb03c915f385150009664a52a38ef
zzkuai <zzkuai@gmail.com>
2022-04-06 22:41:23 +0800

    feat: 调整cors demo

--- server/index.js
+++ server/index.js
@@ -1,37 +1,45 @@
-const http = require("http");
+const express = require('express');
 
-http
-  .createServer((req, res) => {
-    // 非简单请求指：
-    // 1.method 是 GET POST HEAD 且不带自定义request header
-    // 2.content-type 为 application/x-www-form-urlencoded multipart/form-data text/plain
+const app = express();
 
+app.use('*', (req, res, next) => {
+  res.set({
     // 允许跨域请求的前端域名
-    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+    'Access-Control-Allow-Origin': req.headers.origin,
+
+    // 允许跨域请求方法
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
 
     // 非简单请求下允许的额外请求头
-    res.setHeader("Access-Control-Allow-Headers", "content-type, x-key");
+    'Access-Control-Allow-Headers': 'content-type, x-key',
 
     // cookie 设置
-    res.setHeader("Access-Control-Allow-Credentials", true);
+    'Access-Control-Allow-Credentials': true,
 
     // 允许前端获取的响应头
-    res.setHeader("Access-Control-Expose-Headers", "x-version");
+    'Access-Control-Expose-Headers': 'x-version',
 
     // 非简单请求场景下 预检请求的缓存时间 单位 s
-    res.setHeader("Access-Control-Max-Age", 300);
+    'Access-Control-Max-Age': 10,
 
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200, {
-      "Content-Type": "application/json",
+    'Content-Type': 'application/json',
 
-      // 向客户端设置一个Cookie
-      "Set-Cookie": "myCookie=test",
+    // 自定义 res header
+    'x-version': 1.0,
+  });
 
-      // 自定义 res header
-      "x-version": 1.0,
-    });
+  res.cookie('myCookie', 'test');
 
-    res.end(JSON.stringify({ name: "zzkuai", age: 24 }));
-  })
-  .listen(3000, "127.0.0.1");
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    name: 'zzkuai',
+    age: 24,
+  });
+});
+
+app.listen(3000, '127.0.0.1', () => {
+  console.log(`server start`);
+});
